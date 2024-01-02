3ce014d7a37c20796340e79deb2b3e9404dea761
Bryan Choong <bryanchoong12@gmail.com>
2024-01-03 04:33:48 +0800

    cors update

--- src/utils/response.util.js
+++ src/utils/response.util.js
@@ -3,7 +3,7 @@ function addCookie(res, cookieName, cookieValue, cookieAge) {
         httpOnly: true,
         maxAge: cookieAge,
         sameSite: "none",
-        secure: false,
+        secure: true,
     });
 }
 
